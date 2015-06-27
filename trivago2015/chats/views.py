c096d80457f2be37933b225b3cc8237181e292a7
Sumit Datta <sumit.datta@smaato.com>
2015-06-27 19:21:03 +0200

    Chat works

--- trivago2015/chats/views.py
+++ trivago2015/chats/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from trivago2015.events.models import Event
+from .models import Message
+
+
+def get_messages(request, event_id):
+    try:
+        event = Event.objects.get(id=event_id)
+    except Event.DoesNotExist:
+        return JsonResponse(
+            {"error": "Event not found"},
+            status=404
+        )
+
+    return JsonResponse({
+        "count": Message.objects.filter(event=event).count(),
+        "results": [{
+            "text": x.text,
+            "author": x.author.username,
+            "created_at": x.created_at.isoformat()
+        } for x in Message.objects.filter(event=event)]
+    },
+    safe=False)
+
+
+@csrf_exempt
+def post_message(request, event_id):
+    try:
+        event = Event.objects.get(id=event_id)
+    except Event.DoesNotExist:
+        return JsonResponse(
+            {"error": "Event not found"},
+            status=404
+        )
+
+    print(request.method, request.POST)
+    if not request.POST.get('text', None):
+        return JsonResponse(
+            {"error": "Please provide text"}
+        )
+
+    msg = Message()
+    msg.event = event
+    msg.author = request.user
+    msg.text = request.POST.get('text', None)
+    msg.save()
+
+    return JsonResponse({
+        "status": "success",
+        "created_at": msg.created_at
+    })
