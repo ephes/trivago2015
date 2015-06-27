819eca09298c2ebdee7207236346b4684a8e587c
Sumit Datta <sumit.datta@smaato.com>
2015-06-27 14:29:14 +0200

    Added Event and created_at to chats.Message

--- trivago2015/chats/models.py
+++ trivago2015/chats/models.py
@@ -4,4 +4,7 @@ from django.conf import settings
 
 class Message(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='author')
-    text = models.CharField(max_length=500, default=None, null=True)
+    event = models.ForeignKey('events.Event')
+    text = models.CharField(max_length=250, default=None, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
