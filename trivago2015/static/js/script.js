1889ad0cb64be29aa8253030fbd9a4282c507e1c
Sumit Datta <sumit.datta@smaato.com>
2015-06-27 22:40:49 +0200

    Updated frontend app

--- trivago2015/static/js/script.js
+++ trivago2015/static/js/script.js
@@ -17,3 +17,21 @@ $(".back").click(function(){
   var newPageId = '#people-'+newOrder;
   $.mobile.changePage($(newPageId), {transition:"slide"});
 });
+
+
+$("#btn-find-events").click(function() {
+  var prefs = [];
+  $("#preference-list").find("input:checked").each(function() {
+    var id = $(this).attr("id");
+    prefs.push($("#preference-list").find("label[for='" + id + "']").text());
+  });
+  $.ajax("/events/fetch/", {
+    "dataType": "json",
+    "data": JSON.stringify({
+      "preferences": prefs
+    }),
+    "success": function(data) {
+      console.log(data);
+    }
+  })
+});
