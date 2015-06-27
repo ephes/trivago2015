9b67dbf223ccd917409d1c064d152045c0f61d40
Jochen Wersdörfer <jochen@wersdoerfer.de>
2015-06-27 11:50:55 +0200

    set domain and initial migration

--- trivago2015/contrib/sites/migrations/0002_set_site_domain_and_name.py
+++ trivago2015/contrib/sites/migrations/0002_set_site_domain_and_name.py
@@ -11,7 +11,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "waaaasssuuuuppp.com",
+            "domain": "j212975.servers.jiffybox.net",
             "name": "trivago2015"
         }
     )
