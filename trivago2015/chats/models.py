75f7d5408d3d1ae61482bc2c6d15d022d4db20d9
Jochen Wersdörfer <jochen@wersdoerfer.de>
2015-06-27 14:26:40 +0200

    added event fk

--- trivago2015/chats/models.py
+++ trivago2015/chats/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.conf import settings
 
+from trivago2015.events.models import Event
+
 
 class Message(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='author')
+    event = models.ForeignKey(Event, related_name='event')
     text = models.CharField(max_length=500, default=None, null=True)
