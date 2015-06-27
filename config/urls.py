1b5367c0d23907ff0bc676a00f395d5f4f5aeb45
Sumit Datta <sumit.datta@smaato.com>
2015-06-27 19:56:28 +0200

    Frontend HTML/JS app added

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name="home"),
+    url(r'^$', TemplateView.as_view(template_name='pages/index.html'), name="home"),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name="about"),
 
     # Django Admin
