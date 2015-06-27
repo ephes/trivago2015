e13f69553ab79c8b67e2371927594d0856e662ec
Jochen Wersdörfer <jochen@wersdoerfer.de>
2015-06-27 15:52:03 +0200

    fetch events view + title & categories for event model

--- trivago2015/events/views.py
+++ trivago2015/events/views.py
@@ -4,6 +4,8 @@ import logging
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Event
+
 
 logger = logging.getLogger(__name__)
 
@@ -13,6 +15,20 @@ def fetch_events(request):
     Return Events-Resultset in json format.
     """
     logger.info("request post: {}".format(request.POST))
+    logger.info("request get: {}".format(request.GET))
+    categories = set([request.GET["foo"],])
+    logger.info('categories: {}'.format(categories))
+    events = Event.objects.all()
     result = []
+    for event in Event.objects.all():
+        e_categories = set(event.categories.split(','))
+        logger.info("ecategories: {}".format(e_categories))
+        intersection = categories.intersection(e_categories)
+        logger.info("intersection: {}".format(intersection))
+        if len(intersection) > 0:
+            result.append({
+                'title': event.title,
+                'description': event.description,
+            })
     json_response = json.dumps(result)
     return HttpResponse(json_response, content_type="application/json")
