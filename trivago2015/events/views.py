f672c76292f08f4f7e385b18eae45e4dfb30c546
Jochen Wersdörfer <jochen@wersdoerfer.de>
2015-06-28 09:59:38 +0200

    fix

--- trivago2015/events/views.py
+++ trivago2015/events/views.py
@@ -83,10 +83,7 @@ def fetch_events(request):
             intersection = categories.intersection(e_categories)
             logger.info("intersection: {}".format(intersection))
             if len(intersection) > 0:
-                result.append({
-                    'title': event.title,
-                    'description': event.description,
-                })
+                result.append(event_to_dict(event))
     json_response = json.dumps(result)
     logger.info("result: {}".format(result))
     return HttpResponse(json_response, content_type="application/json")
