7f7890c92b837525a5bfef8a85e1f59338a402f7
Sumit Datta <sumit.datta@smaato.com>
2015-06-28 11:22:46 +0200

    Fixed error when no events data; clear all existing events on new request

--- trivago2015/static/js/script.js
+++ trivago2015/static/js/script.js
@@ -1,3 +1,4 @@
+/*
 $(".forward").click(function(){
   var order = $(this).attr("data-order");
   var newOrder = parseInt(order)+1;
@@ -18,15 +19,73 @@ $(".back").click(function(){
   var newPageId = '#people-'+newOrder;
   $.mobile.changePage($(newPageId), {transition:"slide"});
 });
+*/
+
 
 $("#chat-submit").click(function(){
   var textValue = $("#chat-input-form").val();
-  var bubbleMarkup = "<div class='my-blob'>"+textValue+"</div>";
+  var bubbleMarkup = "<div class='my-blob'>" + textValue + "</div>";
   $("#chat-content").append(bubbleMarkup);
   $("#chat-input-form").val("");
 });
 
+var handle_events_data = function(data) {
+  var response = data;
+  for(var i= 0; i < response.length; i++) {
+    var title = response[i].title;
+    var categories = [];
+    if (response[i].categories.length) {
+      var categories = response[i].categories;
+    }
+    var description = response[i].description;
+    var image = response[i].image;
 
+    var categories_str = '';
+    for(var j= 0; j < categories.length; j++) {
+      categories_str = categories_str + '<li><img class="icon" src="/static/images/icons/' + categories[j] + '.png" /></li>';
+    }
+
+    var previous_str = '';
+    if (i != 0) {
+      previous_str = '<a class="back" href="#page-event-' + (i - 1) + '">' +
+        '<img src="/static/images/icons/back.png" />' +
+      '</a>'
+    }
+
+    var next_str = '';
+    if (i != (response.length - 1)) {
+      next_str = '<a class="forward" href="#page-event-' + (i + 1) + '">' +
+        '<img src="/static/images/icons/forward.png" />' +
+      '</a>'
+    }
+
+    var eventMarkup = '<div data-role="page" id="page-event-' + i + '" data-theme="a" class="page-events">' +
+      '<div data-role="header">' +
+        '<a href="#page-browse" class="ui-btn ui-shadow ui-corner-all ui-btn-inline ui-icon-back ui-btn-icon-left"></a>' +
+        '<h1>' + title + '</h1>' +
+      '</div>' +
+      '<!-- /header -->' +
+
+      '<div role="main" class="ui-content" style="padding:0;">' +
+        '<div class="people-image">' +
+          '<img src="' + image + '" />' +
+        '</div>' + previous_str +
+        '<a href="#page-chat">' +
+          '<img class="top" src="/static/images/icons/top.png" />' + next_str +
+        '</a>' +
+        '<div class="people-txt">' +
+          '<p>' + description + '</p>' +
+          '<p class="category-title">Categories:</p>' +
+          '<ul class="categories">' + categories_str + '</ul>' +
+        '</div>' +
+      '</div></div>';
+    $("body").append(eventMarkup);
+  }
+
+  if ($('#page-event-0').length) {
+    $.mobile.changePage($("#page-event-0"), {transition:"slide"});
+  }
+}
 
 $("#btn-find-events").click(function() {
   var prefs = [];
@@ -34,6 +93,7 @@ $("#btn-find-events").click(function() {
     var id = $(this).attr("id");
     prefs.push($("#preference-list").find("label[for='" + id + "']").text());
   });
+  $('div.page-events').remove();
   $.ajax("/events/fetch/", {
     "dataType": "json",
     "method": "POST",
@@ -41,60 +101,6 @@ $("#btn-find-events").click(function() {
       "preferences": prefs
     }),
     "processData": false,
-    "success": function(data) {
-      var response = data;
-      for(var i= 0; i < response.length; i++) {
-        var title = response[i].title;
-        var categories = [];
-        if (response[i].categories.length) {
-          var categories = response[i].categories;
-        }
-        var description = response[i].description;
-        var image = response[i].image;
-
-        var categories_str = '';
-        for(var j= 0; j < categories.length; j++) {
-          categories_str = categories_str + '<li><img class="icon" src="/static/images/icons/' + categories[j] + '.png" /></li>';
-        }
-
-        var previous_str = '';
-        if (i != 0) {
-          previous_str = '<a class="back" href="#page-event-' + (i - 1) + '">' +
-            '<img src="/static/images/icons/back.png" />' +
-          '</a>'
-        }
-
-        var next_str = '';
-        if (i != (response.length - 1)) {
-          next_str = '<a class="forward" href="#page-event-' + (i + 1) + '">' +
-            '<img src="/static/images/icons/forward.png" />' +
-          '</a>'
-        }
-
-        var eventMarkup = '<div data-role="page" id="page-event-' + i + '" data-theme="a" class="page-events">' +
-          '<div data-role="header">' +
-            '<a href="#page-browse" class="ui-btn ui-shadow ui-corner-all ui-btn-inline ui-icon-back ui-btn-icon-left"></a>' +
-            '<h1>' + title + '</h1>' +
-          '</div>' +
-          '<!-- /header -->' +
-
-          '<div role="main" class="ui-content" style="padding:0;">' +
-            '<div class="people-image">' +
-              '<img src="' + image + '" />' +
-            '</div>' + previous_str +
-            '<a href="#page-chat">' +
-              '<img class="top" src="/static/images/icons/top.png" />' + next_str +
-            '</a>' +
-            '<div class="people-txt">' +
-              '<p>' + description + '</p>' +
-              '<p class="category-title">Categories:</p>' +
-              '<ul class="categories">' + categories_str + '</ul>' +
-            '</div>' +
-          '</div></div>';
-        $("body").append(eventMarkup);
-      }
-
-      $.mobile.changePage($("#page-event-0"), {transition:"slide"});
-    }
+    "success": handle_events_data
   })
 });
